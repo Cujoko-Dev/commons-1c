fdb9f03920501f09c17e47c17b2f788b82fec8d1
Cujoko <cujoko@gmail.com>
2018-04-07 11:40:30 +0900

    Переработал setup.py. Добавил requirements.txt.

--- setup.py
+++ setup.py
@@ -5,39 +5,22 @@ import commons_1c
 
 setup(
     name='commons_1c',
-
     version=commons_1c.__version__,
-
     description='Commons for 1C:Enterprise',
-
     url='https://github.com/Cujoko/commons-1c',
-
     author='Cujoko',
     author_email='cujoko@gmail.com',
-
     license='MIT',
-
     classifiers=[
         'Development Status :: 4 - Beta',
-
         'Intended Audience :: Developers',
-
         'License :: OSI Approved :: MIT License',
-
         'Natural Language :: Russian',
-
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-
         'Topic :: Software Development',
         'Topic :: Utilities'
     ],
-
     keywords='1c commons',
-
-    install_requires=[
-        'appdirs'
-    ],
-
     py_modules=['commons_1c']
 )
