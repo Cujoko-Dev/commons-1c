e0b11e3f377f6c5262323005b3d3915beeff9d8f
Cujoko <cujoko@gmail.com>
2018-10-28 04:55:22 +0900

    Изменит в setup.py ссылки на проекты согласно PEP 508

--- setup.py
+++ setup.py
@@ -27,12 +27,9 @@ setup(
     ],
     keywords='1c commons',
     license='MIT',
-    dependency_links=[
-        'https://gitlab.com/Cujoko/commons/-/archive/master/commons-master.tar.gz#egg=commons-2.0.0'
-    ],
     install_requires=[
         'appdirs>=1.4.3',
-        'commons>=2.0.0',
+        'commons>=2.0.0 @ https://gitlab.com/Cujoko/commons/-/archive/master/commons-master.tar.gz#egg=commons-2.1.0',
         'six>=1.11.0'
     ]
 )
